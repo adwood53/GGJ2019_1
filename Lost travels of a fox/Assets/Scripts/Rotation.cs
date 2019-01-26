a42ff887949d99aab2e60d56029f27f3c1e46c54
P16157254 <P16157254@GWHXX631WIN004W>
2019-01-26 20:42:00 +0000

    added jam sandwich

---
+++ Lost travels of a fox/Assets/Scripts/Rotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private GameObject particles;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            particles.SetActive(true);
+            
+            Destroy(this.gameObject, 0.5f);
+        }
+    }
+}
