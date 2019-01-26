3a3c70537809d08d1443794b748baa5eba5b9b65
Edward-Wootten <47054304+Edward-Wootten@users.noreply.github.com>
2019-01-26 12:27:18 +0000

    coding

--- Lost travels of a fox/Assets/Scripts/Interaction/Interaction.cs
+++ Lost travels of a fox/Assets/Scripts/Interaction/Interaction.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interaction : MonoBehaviour {
+public class Interaction : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      /*  var playerObject = GameObject.Find("Player");
+        var NPCObject = GameObject.Find("Interactable");
+        Vector3 playerPosition = playerObject.transform.position;
+        Vector3 NPCPosition = NPCObject.transform.position;
+        float dist = Vector3.Distance(playerPosition, NPCPosition);
+
+        if (dist <= 5 && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("TEXT");
+        }*/
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
 }
